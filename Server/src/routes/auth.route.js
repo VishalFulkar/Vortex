e1d1994bc0ca85c09faaa10a45a2b618dd4b1a04
VishalFulkar <vishalfulkar9575@gmail.com>
2026-06-26 21:55:47 +0530

    feat: remove authentication middleware from routes for user registration and login

--- Server/src/routes/auth.route.js
+++ Server/src/routes/auth.route.js
@@ -1,9 +1,8 @@
 const express = require("express")
 const router = express.Router();
 const { register, login } = require("../controllers/auth.controller");
-const { authUser } = require("../middleware/auth.middleware");
 
-router.post("/register", authUser, register);
-router.post("/login", authUser, login)
+router.post("/register", register);
+router.post("/login", login)
 
 module.exports = router;
